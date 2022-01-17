4765e51d85b25c85eeb9f37492eeafb7086da3d8
Bryce Richardson <drichardson2@harding.edu>
2022-01-17 15:14:10 -0600

    ahh this might not work

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,21 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const port = process.env.PORT || 3000;
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.[hash].tsx',
-  },
+  entry: './src/index.ts',
   module: {
     rules: [
-      // First Rule
       {
-        test: /\.(tsx)$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-
-      // Second Rule
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localsConvention: 'camelCase',
-              sourceMap: true,
-            },
-          },
-        ],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.tsx',
-      favicon: 'public/favicon.ico',
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
